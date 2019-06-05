e4894b4030e5d50c45eda9f82013d67d5f054f30
Lucas Ferrari <lucas.ferrari@sensedia.local>
2019-06-05 09:36:07 -0300

    port adjusts

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var app = require("./custom-express")();
 
-app.listen(8080, function () {
+// app.listen(8080, function () {
+// 	console.log("Server online. Go to http://localhost:8080/geolocations to insert geolocation.");
+// });
+app.listen(process.env.PORT || 8080, function () {
 	console.log("Server online. Go to http://localhost:8080/geolocations to insert geolocation.");
 });
