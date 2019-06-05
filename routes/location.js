5694c0f7170cf8f56a88e6be6356eae55eef0c72
Lucas Ferrari <lucas.ferrari@sensedia.local>
2019-06-05 11:11:15 -0300

    added route

--- routes/location.js
+++ routes/location.js
@@ -13,16 +13,23 @@ module.exports = function (app) {
             res.send(html);
         });
 
+        app.get('/risk_areas', function (req, res) {
+            res.setHeader('Content-Type', 'application/json');
+            res.send(JSON.stringify([{"latitude": -12.158792,"longitude": -76.929747},{"latitude": -12.204663,"longitude": -76.952769},{"latitude": -11.968713,"longitude": -76.993997},{"latitude": -11.792829,"longitude": -76.987340}]));
+        });
+
         // Displays the map.
         app.post('/directions', function (req, res) {
             console.log("Lat= " + req.body.latitude);
             console.log("Lon= " + req.body.longitude);
             var html='';
+            html +="<html>";
             html +="<body>";
             html += "<form>";
             html += "<iframe src='https://embed.waze.com/iframe?zoom=50&lat="+req.body.latitude+"&lon="+req.body.longitude+"&pin=1'width='100%' height='520'></iframe>"; 
             html += "</form>";
             html += "</body>";
+            html += "</html>";
             res.send(html);
         });
 };
